c9df9c53c86477244cfbedf6595581d1783c739e
dpsec26 <254673537+dpsec26@users.noreply.github.com>
2026-03-26 11:48:04 +0100

    chore: automated superuser creation

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 
 python manage.py migrate
 python manage.py collectstatic --noinput
+python manage.py create_superuser_from_env
 
 if [ "${DJANGO_DEBUG}" == "true" ]; then
     python manage.py runserver 0.0.0.0:${BACKEND_PORT}
